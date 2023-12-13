808a578f5a983f0ced0414582deea4e9a7613fd6
github-cloudlabsuser-077 <80263823+github-cloudlabsuser-077@users.noreply.github.com>
2023-12-13 15:52:07 +0000

    added more to index

---
+++ index.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+
+const MarkdownEditor = () => {
+    const [markdown, setMarkdown] = useState('type markdown here');
+
+    const handleMarkdownChange = (event) => {
+        setMarkdown(event.target.value);
+    };
+
+    return (
+        <div>
+            <textarea value={markdown} onChange={handleMarkdownChange} />
+            <ReactMarkdown>{markdown}</ReactMarkdown>
+        </div>
+    );
+};
+
+export default MarkdownEditor;
+
+// reverse a sentence
+// the start of the sentence must be capitalized
+const reverseSentence = (sentence) => {
+    const words = sentence.split(' ');
+    const reversedWords = words.map((word) => word.split('').reverse().join(''));
+    const reversedSentence = reversedWords.join(' ');
+    return reversedSentence.charAt(0).toUpperCase() + reversedSentence.slice(1);
+};
+
+const data = [
+        [
+            { name: 'John', age: 25 },
+            { name: 'Jane', age: 30 }
+        ],
+        [
+            { name: 'Bob', age: 40 }
+        ]
+];
+
+const names = data.flatMap((group) => group.map((person) => person.name));
+console.log(names);
+// Output: ['John', 'Jane', 'Bob']
